bc95cd954afcdf0682a3ae7294c089d878b64f68
梁晨 <1161480992@qq.com>
2023-10-18 20:56:04 +0800

    布局调整

--- src/page/component/index.vue
+++ src/page/component/index.vue
@@ -2,7 +2,7 @@
   <div style="display: flex; height: 100%">
     <div style="height: 100%;padding-left: 300px;"><ViMenu v-model="menuData" :data="data"></ViMenu></div>
 
-    <div style="padding: 15px;  height: calc(100vh - 60px)">
+    <div style="padding: 15px;  height: calc(100vh - 60px);width: 100%;overflow-y: scroll;">
       <RouterView></RouterView>
     </div>
   </div>
