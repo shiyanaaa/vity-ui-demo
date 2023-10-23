87bdfda217b67bd3433c1c86e2c23a6c98b8c31d
梁晨 <1161480992@qq.com>
2023-10-23 21:38:43 +0800

    配置vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:'/vity-ui-demo',
-  plugins: [vue()],
-  server: {
-    // ← ← ← ← ← ←
-    host: '0.0.0.0' // ← 新增内容 ←
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({mode}:{mode:string} )=> {
+  return defineConfig({
+    base:loadEnv(mode, process.cwd()).BASE_URL,
+    plugins: [vue()],
+    server: {
+      // ← ← ← ← ← ←
+      host: '0.0.0.0' // ← 新增内容 ←
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  })
+}
