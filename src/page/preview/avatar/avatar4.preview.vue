62706de37bd84d26ada3235857880846f5df8989
梁晨 <1161480992@qq.com>
2023-11-02 15:00:31 +0800

    avatar路径再次修改

--- src/page/preview/avatar/avatar4.preview.vue
+++ src/page/preview/avatar/avatar4.preview.vue
@@ -4,7 +4,7 @@
 </preview>
 <template>
   <div class="item">
-    <ViAvatar size="80" v-for="fit in fits" :key="fit" :fit="fit" shape="square" src="/imgs/demo.jpg" />
+    <ViAvatar size="80" v-for="fit in fits" :key="fit" :fit="fit" shape="square" src="/vity-ui-demo/imgs/demo.jpg" />
   </div>
 </template>
 <script setup lang="ts">
